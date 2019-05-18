98be11e490b18d70b97e9f971915cd58e61bb4c7
Adrienne <adrienneeaston@gmail.com>
2019-05-17 18:06:35 -0700

    responsive css

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,12 @@
 //   $(this).next().removeClass('hide-me');
 // });
 
-$(".button-detail").click(function(){
-  $("#form-details").toggle();
+// $(".button-detail").click(function(){
+//   $("#form-details").toggle();
+// });
+
+(document).ready(function() {
+  $('.button-detail').on('click', function(slide){
+    $(slide.target).next().toggle(this);
+  });
 });
